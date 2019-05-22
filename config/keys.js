34ebcd20772054cfa17fa26da5a131ccf8815d82
Mike <mikeinhistory@gmail.com>
2019-05-21 19:34:14 -0500

    small changes to sendgrid

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@ exports.secrets = {
     // YelpAPI_KEY: process.env.YelpAPI_KEY,
     secretOrKey: process.env.secretOrKey,
     // GoogleAPI_KEY: process.env.GoogleAPI_KEY,
-    // sendgridusername: process.env.sendgridusername,
-    // sendgridpass:  process.env.sendgridpass
+    sendgridusername: process.env.sendgridusername,
+    sendgridpass:  process.env.sendgridpass
 }
