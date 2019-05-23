707282960f23e4d5290eb6b4b424b96d3de69895
Josh Guerra <joshua.guerra21@gmail.com>
2019-05-22 19:50:16 -0500

    Heroku Fix maybe

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Defines middleware
 app.use(express.urlencoded({ extended: true }));
